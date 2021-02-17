cf9e60057e675de0ba110ef8609918ba7464f0aa
Felipe Milana <felipe.milana97@gmail.com>
2021-02-17 17:55:26 -0300

    Project created

---
+++ src/main/java/com/javaudemy/SpringBoot_Ionic/SpringBootIonicApplication.java
@@ -0,0 +1,13 @@
+package com.javaudemy.SpringBoot_Ionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaudemy/SpringBoot_Ionic/SpringBootIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaudemy.SpringBoot_Ionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
