02a7c22b5fa58cc1270144a3202aa5ed38ed5beb
Felipe Milana <felipe.milana97@gmail.com>
2021-02-18 17:12:32 -0300

    Category class, repository, service and resource

---
+++ src/main/java/com/javaudemy/SpringBoot_Ionic/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.javaudemy.SpringBoot_Ionic.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.javaudemy.SpringBoot_Ionic.domain.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+
+}
---
+++ src/main/java/com/javaudemy/SpringBoot_Ionic/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.javaudemy.SpringBoot_Ionic.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javaudemy.SpringBoot_Ionic.domain.Category;
+import com.javaudemy.SpringBoot_Ionic.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	public CategoryRepository repository;
+	
+	public List<Category> findAll()	{
+		return repository.findAll();
+	}
+	
+	public Category findById(Integer id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+}
