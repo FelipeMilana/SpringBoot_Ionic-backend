cf362f7fffa204f1661a081643571c02d7275683
Felipe Milana <felipe.milana97@gmail.com>
2021-03-10 15:26:42 -0300

    Implementing SmtpEmailService

---
+++ src/main/java/com/javaudemy/SpringBoot_Ionic/services/SmtpEmailService.java
@@ -0,0 +1,22 @@
+package com.javaudemy.SpringBoot_Ionic.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class SmtpEmailService extends AbstractEmailService{
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
+	
+	@Override
+	public void sendEmail(SimpleMailMessage mail) {
+		LOG.info("Enviando email..");
+		javaMailSender.send(mail);
+		LOG.info("Email enviado!");
+	}
+}
