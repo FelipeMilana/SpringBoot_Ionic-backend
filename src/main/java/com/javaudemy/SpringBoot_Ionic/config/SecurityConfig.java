822f64c5342ef7f7016bd3b04348bb8b94fad7b9
Felipe Milana <felipe.milana97@gmail.com>
2021-03-16 17:39:35 -0300

    Implementing Authorization

--- src/main/java/com/javaudemy/SpringBoot_Ionic/config/SecurityConfig.java
+++ src/main/java/com/javaudemy/SpringBoot_Ionic/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.javaudemy.SpringBoot_Ionic.security.JWTAuthenticationFilter;
+import com.javaudemy.SpringBoot_Ionic.security.JWTAuthorizationFilter;
 import com.javaudemy.SpringBoot_Ionic.security.JWTUtil;
 
 @Configuration
@@ -58,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
