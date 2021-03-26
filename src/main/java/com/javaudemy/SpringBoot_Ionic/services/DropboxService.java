5d880b48dbb274e95e42550967f66cdbd52899a5
Felipe Milana <felipe.milana97@gmail.com>
2021-03-26 14:53:17 -0300

    DropboxService - deleteFile method

--- src/main/java/com/javaudemy/SpringBoot_Ionic/services/DropboxService.java
+++ src/main/java/com/javaudemy/SpringBoot_Ionic/services/DropboxService.java
@@ -19,6 +19,7 @@ import com.dropbox.core.v2.files.ListFolderErrorException;
 import com.dropbox.core.v2.files.ListFolderResult;
 import com.dropbox.core.v2.files.Metadata;
 import com.dropbox.core.v2.files.WriteMode;
+import com.dropbox.core.v2.sharing.SharedLinkErrorException;
 import com.dropbox.core.v2.sharing.SharedLinkMetadata;
 import com.javaudemy.SpringBoot_Ionic.services.exceptions.FileException;
 
@@ -26,85 +27,108 @@ import com.javaudemy.SpringBoot_Ionic.services.exceptions.FileException;
 public class DropboxService {
 
 	private Logger LOG = LoggerFactory.getLogger(DropboxService.class);
-	
+
 	@Autowired
 	private DbxClientV2 dbxClient;
-	
+
 	public URI uploadFile(MultipartFile file) {
 		try {
 			String fileName = file.getOriginalFilename();
 			InputStream inputStream = file.getInputStream();
 			return uploadFile(inputStream, fileName);
-		} 
-		catch (IOException e) {
+		} catch (IOException e) {
 			throw new FileException("Erro de IO: " + e.getMessage());
 		}
-		
+
 	}
-	
+
 	public URI uploadFile(InputStream is, String fileName) {
 		try {
 			LOG.info("Iniciando upload");
-			FileMetadata metadata = dbxClient.files()
-					.uploadBuilder("/" +fileName)
-					.withAutorename(true)
-					.withMode(WriteMode.ADD)
-					.uploadAndFinish(is);
+			FileMetadata metadata = dbxClient.files().uploadBuilder("/" + fileName).withAutorename(true)
+					.withMode(WriteMode.ADD).uploadAndFinish(is);
 			LOG.info("Upload feito");
 			String url = dbxClient.sharing().createSharedLinkWithSettings(metadata.getId()).getUrl();
-			
-			//cors configuration
+
+			// cors configuration
 			String newUrl = "https://dl.dropboxusercontent.com" + url.substring(23);
-			
+
 			LOG.info("URL gerada");
 			return new URI(newUrl);
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			throw new FileException("Erro de IO: " + e.getMessage());
-		}  
-		catch (DbxException e) {
+		} catch (DbxException e) {
 			throw new FileException("Erro de Dbx: " + e.getMessage());
-		} 
-		catch (URISyntaxException e) {
+		} catch (URISyntaxException e) {
 			throw new FileException("Erro de URISyntax: " + e.getMessage());
 		}
 	}
-	
+
 	public URI getUriFromFile(String fileName) {
 		try {
 			ListFolderResult result = dbxClient.files().listFolder("");
-		
-			for(Metadata metadata: result.getEntries()) {
-				if(metadata.getName().equalsIgnoreCase(fileName)) { 
-					
+
+			for (Metadata metadata : result.getEntries()) {
+				if (metadata.getName().equalsIgnoreCase(fileName)) {
+
 					String url = null;
-					
-					List<SharedLinkMetadata> list = dbxClient.sharing().listSharedLinksBuilder().withPath(((FileMetadata) metadata).getId()).start().getLinks();
-					for(SharedLinkMetadata shared: list) {
+
+					List<SharedLinkMetadata> list = dbxClient.sharing().listSharedLinksBuilder()
+							.withPath(((FileMetadata) metadata).getId()).start().getLinks();
+					for (SharedLinkMetadata shared : list) {
 						url = shared.getUrl();
 					}
-					
-					if(url == null) {
-						url = dbxClient.sharing().createSharedLinkWithSettings(((FileMetadata) metadata).getId()).getUrl();
+
+					if (url == null) {
+						url = dbxClient.sharing().createSharedLinkWithSettings(((FileMetadata) metadata).getId())
+								.getUrl();
 						LOG.info("URL gerada");
 					}
-					
-					//cors configuration
+
+					// cors configuration
 					String newUrl = "https://dl.dropboxusercontent.com" + url.substring(23);
-					
+
 					return new URI(newUrl);
 				}
 			}
 			return null;
-		} 
-		catch (ListFolderErrorException e) {
+		} catch (ListFolderErrorException e) {
 			throw new FileException("Erro de ListFolderError: " + e.getMessage());
-		} 
-		catch (DbxException e) {
+		} catch (DbxException e) {
 			throw new FileException("Erro de Dbx: " + e.getMessage());
-		} 
-		catch (URISyntaxException e) {
+		} catch (URISyntaxException e) {
 			throw new FileException("Erro de URISyntax: " + e.getMessage());
 		}
-	}	
+	}
+
+	public void deleteFile(String url) {
+		try {
+			//changing to standard dropbox url
+			url = "https://www.dropbox.com" + url.substring(33);
+
+			SharedLinkMetadata metadata = dbxClient.sharing().getSharedLinkMetadata(url);
+			
+			if(!metadata.getName().equalsIgnoreCase("blank-avatar.jpg") 
+					&& !metadata.getName().equalsIgnoreCase("cat.jpg") 
+					&& !metadata.getName().equalsIgnoreCase("prod.jpg")) {
+				
+				dbxClient.files().deleteV2(metadata.getId());
+				LOG.info("Imagem deletada");
+			}
+			else {
+				LOG.info("Imagem não deletada");
+			}
+		}
+		catch(SharedLinkErrorException e) {
+			throw new FileException("Erro de SharedLinkError: " + e.getMessage());
+		}
+		catch(DbxException e) {
+			throw new FileException("Erro de Dbx: " + e.getMessage());
+		}
+		
+		
+		
+		
+		
+	}
 }
