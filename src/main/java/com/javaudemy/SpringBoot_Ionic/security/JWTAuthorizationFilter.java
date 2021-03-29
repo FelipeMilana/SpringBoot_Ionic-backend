9fae7898615e63c954803024864dcdd634d6c051
Felipe Milana <felipe.milana97@gmail.com>
2021-03-29 16:12:34 -0300

    Location header exposure

--- src/main/java/com/javaudemy/SpringBoot_Ionic/security/JWTAuthorizationFilter.java
+++ src/main/java/com/javaudemy/SpringBoot_Ionic/security/JWTAuthorizationFilter.java
@@ -30,7 +30,7 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 									HttpServletResponse response,
 									FilterChain chain) throws IOException, ServletException {
 		
-		String header = request.getHeader("authorization");
+		String header = request.getHeader("Authorization");
 		
 		if(header != null && header.startsWith("Bearer ")) {
 			UsernamePasswordAuthenticationToken auth = getAuthentication(header.substring(7));
